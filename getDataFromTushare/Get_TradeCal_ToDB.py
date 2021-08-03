e3df5058bad10f56b37f187e56c30a942dbf91bf
li6612773steven <Waitingyou6612773>
2021-08-04 02:14:27 +0800

    20210204

---
+++ getDataFromTushare/Get_TradeCal_ToDB.py
@@ -0,0 +1,88 @@
+'''
+Created on 2021年8月04日
+
+@author: SJLI
+
+交易日历
+接口：trade_cal
+描述：获取各大交易所交易日历数据,默认提取的是上交所
+
+输入参数
+
+名称	类型	必选	描述
+exchange	str	N	交易所 SSE上交所,SZSE深交所,CFFEX 中金所,SHFE 上期所,CZCE 郑商所,DCE 大商所,INE 上能源,IB 银行间,XHKG 港交所
+start_date	str	N	开始日期 （格式：YYYYMMDD 下同）
+end_date	str	N	结束日期
+is_open	str	N	是否交易 '0'休市 '1'交易
+输出参数
+
+名称	类型	默认显示	描述
+exchange	str	Y	交易所 SSE上交所 SZSE深交所
+cal_date	str	Y	日历日期
+is_open	str	Y	是否交易 0休市 1交易
+pretrade_date	str	N	上一个交易日
+接口示例
+
+
+pro = ts.pro_api()
+
+
+df = pro.trade_cal(exchange='', start_date='20180101', end_date='20181231')
+或者
+
+
+df = pro.query('trade_cal', start_date='20180101', end_date='20181231')
+'''
+import datetime
+
+import pandas as pd
+import tushare as ts
+from sqlalchemy import create_engine
+from sqlalchemy.types import NVARCHAR,DATE,Integer,DECIMAL
+
+
+def init():
+    # 初始化pro接口
+    token = '416ae9fd9b5e3c1180827bd24f729982057f67cbfd75af9df7fe9a87'
+    print(ts.__version__)
+    # 设置token
+    ts.set_token(token)
+    # 初始化数据库
+    connect_info = 'mysql+pymysql://root:12345@localhost:3306/qtdb' \
+                   ''
+    engine = create_engine(connect_info)  # use sqlalchemy to build link-engine
+    print(engine, '数据库链接初始化成功')
+    return engine
+
+
+# def read_data():
+#     sql = """SELECT * FROM stock_basic LIMIT 20"""
+#     df = pd.read_sql_query(sql, engine)
+#     return df
+
+
+def write_data(df,engine):
+    res = df.to_sql('hq_trade_cal', engine, index=False, if_exists='append', chunksize=10000,
+                    dtype={'exchange': NVARCHAR(20),
+                           'cal_date': DATE,
+                           'is_open': NVARCHAR(1)})
+    print(res)
+
+
+def get_data():
+    currentDate = datetime.datetime.now().strftime('%Y%m%d')
+    pro = ts.pro_api()
+    df = pro.query('trade_cal', start_date='20180101', end_date=currentDate)
+    return df
+
+
+def droptable():
+    engine.execute('drop table hq_trade_cal')
+
+if __name__ == '__main__':
+    # df = read_data()
+    engine = init()
+    droptable()
+    df = get_data()
+    write_data(df, engine)
+    print(df)
--- sqlFile/CL_2.sql
+++ sqlFile/CL_2.sql
@@ -39,9 +39,14 @@ SELECT * FROM qtdb.hq_TopInst order by  trade_date desc;
 --  ----------------------------------------------回测-------------------------------------------------------
 -- 将选出的证券进行回测,选出T+1和T+2的行情
 -- 先缓存一张两日关联信息表
+drop table cl2_hc_T0
+;
 drop table cl2_hc_T1
 ;
 drop table cl2_hc_T2
+;
+create table cl2_hc_T0
+select concat(ts_code,xgrq) as glxx,xgrq from cl2_cl_jg
 ;
 create table cl2_hc_T1
 select concat(ts_code,date_sub(xgrq,interval -1 day)) as glxx,xgrq from cl2_cl_jg
@@ -49,39 +54,47 @@ select concat(ts_code,date_sub(xgrq,interval -1 day)) as glxx,xgrq from cl2_cl_j
 create table cl2_hc_T2
 select concat(ts_code,date_sub(xgrq,interval -2 day)) as glxx,xgrq from cl2_cl_jg
 ;
+select * from cl2_hc_T0 order by xgrq desc
+;
 select * from cl2_hc_T1 order by xgrq desc
 ;
 select * from cl2_hc_T2 order by xgrq desc
 ;
-drop table cl2_hc_jg
+drop table cl2_hc_jg_tn
 ;
--- 取T+1行情，有个问题，下一个自然日不一定是工作日，所以会少选一些出来
-create table cl2_hc_jg
+-- 取T0、T1、T2行情，有个问题，下一个自然日不一定是工作日，所以会少选一些出来
+create table cl2_hc_jg_tn
+SELECT a.*,trade_date as xgrq,'T0' as tn
+    FROM hq_daily a
+    where a.trade_date > '2021-01-01'
+      and concat(a.ts_code, a.trade_date) in (select glxx from cl2_hc_T0 )
+;
+insert into cl2_hc_jg_tn
 SELECT a.*,date_sub(trade_date,interval 1 day) as xgrq,'T1' as tn
     FROM hq_daily a
     where a.trade_date > '2021-01-01'
       and concat(a.ts_code, a.trade_date) in (select glxx from cl2_hc_T1 )
 ;
-insert into cl2_hc_jg
+insert into cl2_hc_jg_tn
 SELECT a.*,date_sub(trade_date,interval 2 day) as xgrq,'T2' as tn
     FROM hq_daily a
     where a.trade_date > '2021-01-01'
       and concat(a.ts_code, a.trade_date) in (select glxx from cl2_hc_T2 )
 ;
-select * from cl2_hc_jg order by xgrq desc
+select * from cl2_hc_jg_tn order by xgrq desc
 ;
 -- 将新股标识出来，标出上市多少天了 ，除开了新上市的票，提升不明显，降低也不明显，可以踢掉新发的涨停票，避免第二天买不到
-drop table cl2_hc_jg1
+drop table cl2_hc_jg_tn_t1
 ;
-create table cl2_hc_jg1
+create table cl2_hc_jg_tn_t1
 select a.*,b.name, area, industry, market, list_date,datediff(current_date(),list_date) as IPOnDay
-from cl2_hc_jg a left join  hq_stock_basic b on a.ts_code = b.ts_code    order by a.xgrq desc
+from cl2_hc_jg_tn a left join  hq_stock_basic b on a.ts_code = b.ts_code    order by a.xgrq desc
 ;
-select * from cl2_hc_jg1 order by xgrq desc
+select * from cl2_hc_jg_tn_t1 order by xgrq desc
 
 -- 组成包含t1 t2 两天的数据结果
-drop table cl2_hc_jg_big;
-create table cl2_hc_jg_big
+drop table cl2_hc_jg_tn_big;
+create table cl2_hc_jg_tn_big
 select t1.*,
     t2.`ts_code` as t2_ts_code,
     t2.`trade_date` as t2_trade_date,
@@ -102,12 +115,15 @@ select t1.*,
     t2.`market`as t2_market,
     t2.`list_date`as t2_list_date,
     t2.`IPOnDay` as t2_IPOnDay
-       from (select *from cl2_hc_jg1 where tn= 'T1') t1 left join (select *from cl2_hc_jg1 where tn= 'T2') t2 on t1.xgrq = t2.xgrq and t1.ts_code = t2.ts_code
+       from (select *from cl2_hc_jg_tn_t1 where tn= 'T1') t1 left join (select *from cl2_hc_jg_tn_t1 where tn= 'T2') t2 on t1.xgrq = t2.xgrq and t1.ts_code = t2.ts_code
 ;
 -- 在选股第二天，能够成功以昨收价买入的，且以收盘价卖出后的利润率 （除去一年内的新股，只选深圳主板的票）
-drop table cl2_hc_jg_lrl ;
-create table cl2_hc_jg_lrl ;
-select *,(close-pre_close)*100/pre_close as lrl,(cl2_hc_jg_big.t2_close-pre_close)*100/pre_close as lrl2  from cl2_hc_jg_big where tn= 'T1'and IPOnDay>300 and pre_close >low  and substr(ts_code,1,2) in('00','60') order by xgrq desc ;
+drop table cl2_hc_jg_tn_lrl ;
+create table cl2_hc_jg_tn_lrl
+select *,(close-pre_close)*100/pre_close as lrl,(cl2_hc_jg_tn_big.t2_close-pre_close)*100/pre_close as lrl2  from cl2_hc_jg_tn_big where tn= 'T1'and IPOnDay>300 and pre_close >low  and substr(ts_code,1,2) in('00','60')
+and name not like '%ST%' order by xgrq desc ;
+
+select * from cl2_hc_jg_tn_lrl;
 
 
 
